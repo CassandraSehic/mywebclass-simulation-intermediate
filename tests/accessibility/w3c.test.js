f2f0cbe5dc17a82a10c11b233d0d40ff62e35b6e
CassandraSehic <cns27@njit.edu>
2023-03-22 20:33:42 -0400

    work in progress

--- tests/accessibility/w3c.test.js
+++ tests/accessibility/w3c.test.js
@@ -1,23 +1,20 @@
 const { test, expect } = require('@playwright/test');
-const { getViolations } = require('axe-core');
 
 test('W3C validation', async ({ page }) => {
   // Navigate to the web page you want to test
-  await page.goto('http://localhost:3000');
+  await page.goto('https://www.example.com');
 
   // Check whether the page is valid HTML using the W3C validator
-  const response = await page.goto('https://validator.w3.org/nu/', {
+  const response = await page.goto('https://validator.nu/?out=json', {
     method: 'POST',
     headers: {
       'Content-Type': 'text/html; charset=utf-8',
     },
     body: await page.content(),
   });
-  const validationResult = await response.json();
-  const errorCount = validationResult.messages.filter(message => message.type === 'error').length;
+  const validationResult = await response.text();
+  // const errorCount = validationResult.messages.filter(message => message.type === 'error').length;
+  const errorCount = (validationResult.includes("Error") ? 1 : 0)
   await expect(errorCount).toBe(0);
 
-  // Check whether the page meets accessibility standards using Axe
-  const violations = await getViolations(await page.evaluate(() => axe.run()));
-  await expect(violations.length).toBe(0);
 });
