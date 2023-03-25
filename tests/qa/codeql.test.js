ecb21f78f9c629f1758814f5cd1c1922360c8a9a
davidfelix <ddf26@njit.edu>
2023-03-25 17:47:49 -0400

    updated codeql test

--- tests/qa/codeql.test.js
+++ tests/qa/codeql.test.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test.describe('CodeQL Test', () => {
   test('Should not have any code vulnerabilities', async ({ page }) => {
-    await page.goto('http:localhost:3000');
+    await page.goto('http://localhost:3000');
     const vulnerabilities = await page.locator('css=div.vulnerability').count();
     expect(vulnerabilities).toBe(0);
   });
